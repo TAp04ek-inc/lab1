935d1c2d5b30a1dc3895dadf68f4c81eadec4db8
Kusonori <56480413+Kus0nori@users.noreply.github.com>
2021-02-09 14:18:06 +0200

    fix
    added return to functions

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,7 +1,5 @@
 #include "main.hpp"
 
 SearchResult FindMinValue(ArrayPointer array){
-    for (int i = 0; i < ; ++i) {
-        
-    }
+    return {};
 }
--- random_io.cpp
+++ random_io.cpp
@@ -2,4 +2,5 @@
 #include <random>
 ArrayPointer RandomInput(){
     rand(); //hello there
+    return {};
 }
