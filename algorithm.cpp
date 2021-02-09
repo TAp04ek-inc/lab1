8be84aa73677fa9bfeb7757eb67115a971d092da
knopo4ka <annaomelchenko2002@gmail.com>
2021-02-09 20:51:26 +0200

    find min value

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,5 +1,19 @@
 #include "main.hpp"
 
 SearchResult FindMinValue(ArrayPointer array){
-    return {};
+
+    int min = array.Pointer[0], element = 0;
+    for (int i = 0; i < array.Size; i++)
+    {
+        if (array.Pointer[i] < min)
+        {
+            min = array.Pointer[i];
+            element = i;
+        }
+    }
+    SearchResult result;
+
+    result.Value = min;
+    result.Index = element;
+    return result;
 }
