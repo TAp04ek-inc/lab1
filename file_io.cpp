ad582deae67b3b51eed16b85c8740a3b6998afdb
Kusonori <56480413+Kus0nori@users.noreply.github.com>
2021-02-10 03:03:41 +0200

    added file input
    file input

--- file_io.cpp
+++ file_io.cpp
@@ -1,5 +1,56 @@
 #include "main.hpp"
+#include <string>
+#include <fstream>
+#include <iostream>
+using namespace std;
 
 ArrayPointer FileInput(){
-    return {};
+    ArrayPointer array;
+    string numbers[20];
+    puts("Enter your filename");
+    string fileName;
+    ifstream File;
+    for(;;){
+        cin >> fileName;
+        File.open(fileName);
+        if(File.is_open())
+            break;
+        else {
+            puts("Wrong filename, try again!");
+            continue;
+        }
+    }
+    puts("How are your numbers written?\n"
+         "1 - {1,2,3,4...}\n"
+         "2 - {1;2;3;4...}\n"
+         "3 - One number on each string");
+    int choice;
+    char separator;
+    cin >> choice;
+    switch (choice) {
+        case 1:
+            separator = ',';
+            break;
+        case 2:
+            separator = ';';
+            break;
+        case 3:
+            separator = '\n';
+            break;
+        default:
+            separator = ',';
+            puts("Invalid choice, your separator will be automatically set to "",""");
+            break;
+    }
+    int i = 0;
+    while (getline(File, numbers[i], separator)) {
+        array.Size = i++;
+    }
+    array.Pointer = new int[array.Size];
+    for (int j = 0; j < array.Size; ++j) {
+        array.Pointer[j] = stoi(numbers[j]);
+    }
+
+    File.close();
+    return array;
 }
